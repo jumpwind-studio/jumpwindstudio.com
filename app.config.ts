a3bfb8680dcf9302b5265448e506bb5f5fe6d4e2
timredd <timredd13@gmail.com>
2024-11-12 13:30:48 -0500

    Add cloudflare-pages nitro preset

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
+  server: {
+    preset: "cloudflare-pages",
+    minify: false,
+  },
   vite: {
     server: {
       preset: "cloudflare-pages",
