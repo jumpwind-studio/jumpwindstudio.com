89f773015b68b23e7b1c78415c844739fe7da9ff
timredd <timredd13@gmail.com>
2024-12-28 15:44:38 -0500

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,9 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "@solidjs/start/config";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   server: {
@@ -6,6 +11,11 @@ export default defineConfig({
     minify: false,
   },
   vite: {
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
+    },
     server: {
       preset: "cloudflare-pages",
       minify: false,
